8a68cfe24a332cda0681bc0af598acffbbdc71ba
RudychukDmytro <dmytrorudychuk@gmail.com>
2023-08-02 17:41:39 +0300

    Leson_one

--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+def get_films():
+    return [
+        {
+            'id': 1,
+            'title': 'Harry Potter and the Philosoper`s Stone',
+            'release_date': 'November 4, 2001'
+        },
+        {
+            'id': 2,
+            'title': 'Harry Potter and the Chamber of Secrets',
+            'release_date': 'November 3, 2002'
+        },
+        {
+            'id': 3,
+            'title': 'Harry Potter and the Prisoner of Azkaban',
+            'release_date': 'May 31, 2004'
+        },
+        {
+            'id': 4,
+            'title': 'Harry Potter and the Goblet of Fire',
+            'release_date': 'November 18, 2005'
+        }
+    ]
 
 
 @app.route("/")
 @app.route("/hello")
 def index():
-    return '<h1>Hello, world!</h1>'
+    films = get_films()
+    return render_template("hello.html", films=films)
 
 @app.route("/about")
 def about():
-    return '<h1>About</h1>'
+    return render_template("about.html", title='About')
 
 @app.route("/name/<string:name>")
 def name(name: str):
